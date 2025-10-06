6a65af27f4193c20e7eb32b9548c222351f809f3
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-07 03:04:25 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,32 @@
-const CACHE_NAME = "alti-drink-cache-v1";
+const CACHE_NAME = "alti-drink-v1";
 const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "image/icon-192.png",
-  "image/icon-512.png"
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.filter((name) => name !== CACHE_NAME)
+        .map((name) => caches.delete(name))
+      )
+    )
+  );
 });
