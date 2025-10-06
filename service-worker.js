4eed93c1cc496d1f9b4f901972706b29278d0293
adjustment-commits <adjustment.h.nishimoto@gmail.com>
2025-10-06 23:45:12 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,39 @@
-const CACHE_NAME = "alti-v1";
+const CACHE_NAME = "alti-drinking-v3";
 const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "image/slow-emo.png",
-  "image/fast-cool.png",
-  "image/fast-emo.png",
-  "image/slow-cool.png",
-  "image/icon-192.png",
-  "image/icon-512.png"
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./image/icon-192.png",
+  "./image/icon-512.png",
+  "./image/splash-1170x2532.png",
+  "./image/splash-2048x2732.png",
+  "./image/slow-emo.png",
+  "./image/fast-emo.png",
+  "./image/slow-cool.png",
+  "./image/fast-cool.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+// インストール時にキャッシュ
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// オフライン時のフェッチ処理
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 古いキャッシュ削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
